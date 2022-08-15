717fe28823353605d8a39e8ad5c9d87c229a4f9c
PyCaramba <counter088@gmail.com>
2022-08-15 13:40:53 +0300

    Added API documentation. Added url to API docs in footer. Replaced the favicon to static folder.

--- auctions_api/urls.py
+++ auctions_api/urls.py
@@ -1,9 +1,26 @@
 from django.urls import include, path, re_path
 
 from rest_framework.routers import DefaultRouter
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from .views import CategoryViewSet, ListingViewSet, WatchlistViewSet, BidViewSet, CommentViewSet
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Auctions Box API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
@@ -18,4 +35,8 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
     path('auth/session/', include('rest_framework.urls')),
+
+    path('docs.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- commerce/settings.py
+++ commerce/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
+    'drf_yasg',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -131,10 +132,10 @@ DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     ),
 }
