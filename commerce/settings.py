8459cece02f92fb857749827662fade5b71d7ed1
PyCaramba <counter088@gmail.com>
2022-12-05 23:45:00 +0200

    Added some improvements.

--- commerce/settings.py
+++ commerce/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'djoser',
     'drf_yasg',
     'mathfilters',
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
