0b51a328439193d2815dec07b5e920de05fa5e0d
PyCaramba <counter088@gmail.com>
2022-12-06 17:31:34 +0200

    Modified the docker-compose file. Added CSRF trusted origins and modified DEBUG variable.

--- commerce/settings.py
+++ commerce/settings.py
@@ -12,9 +12,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS=['https://*.up.railway.app',]
 
 
 # Application definition
@@ -146,10 +148,8 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 20,
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        # 'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
     ),
 }
